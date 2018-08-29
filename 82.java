a7d03a3d681cee847436c8df5d2af0888bb05ef2
satyachitti <42774533+satyachitti@users.noreply.github.com>
2018-08-29 06:45:44 +0100

    Create 82.java

---
+++ 82.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+ 
+		Scanner s=new Scanner(System.in);
+		double l=s.nextFloat();
+		double b=s.nextFloat();
+		double area=l*b;
+		System.out.printf("%.5f",area);
+	}
+}
